903f6973641bd9749bf46236b2829d97b8b647d1
Jeffrey Gordon <jbgordon00@gmail.com>
2024-05-03 14:41:06 -0400

    Change stuff
    Add gitignore and remove the ipynb file
    
    Add legend

--- sketch.js
+++ sketch.js
@@ -1,17 +1,30 @@
 let freqSlider, sampleRateSlider;
 let displayRatio = 20000; // each pixel represents a small fraction of time
+let sampleStrokeWeight = 9;
+let inputColor, reconstructionColor;
 
 function setup() {
-    freqSlider = createSlider(50, 400);
-    sampleRateSlider = createSlider(500, 800, 800);
-    freqSlider.input(sketch);
-    sampleRateSlider.input(sketch)
-
     let mainCanvas = createCanvas(1000, 600);
-    mainCanvas.parent('sketch-container');
+    mainCanvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+
+    renderSliderSection();
+
+    inputColor = color(31, 147, 242);
+    reconstructionColor = color(245, 158, 66);
 
     translate(10, height / 2);
     sketch();
+}
+
+function renderSliderSection() {
+    freqSlider = createSlider(50, 400);
+    sampleRateSlider = createSlider(500, 800, 800);
+
+    freqSlider.position((windowWidth - width) / 2 + width * 0.1, 40);
+    sampleRateSlider.position((windowWidth - width) / 2 + width * 0.5, 40);
+
+    freqSlider.input(sketch);
+    sampleRateSlider.input(sketch);
 }
 
 function sketch() {
@@ -30,6 +43,7 @@ function sketch() {
 
     drawReconstruction(reconstructedFreq);
     drawSamples(samples);
+    drawInfo(reconstructedFreq);
     drawLegend();
 }
 
@@ -37,12 +51,12 @@ function drawAxis() {
     strokeWeight(1);
     stroke('grey');
     line(0, 0, width, 0);
-    line(0, height / 2, 0, - height / 2);
+    line(0, height / 2, 0, - height / 2); // todo: remove this axis probably - it looks kinda bad
 }
 
 function drawInput() {
     strokeWeight(3);
-    stroke(31, 147, 242);
+    stroke(inputColor);
     drawSineWave(freqSlider.value(), 0);
 }
 
@@ -76,7 +90,7 @@ function computeSamples() {
 
 function drawSamples(samples) {
     stroke('white');
-    strokeWeight(9);
+    strokeWeight(sampleStrokeWeight);
 
     for (let samp = 0; samp < samples.length; samp++) {
         t = samp * 1 / sampleRateSlider.value();
@@ -107,7 +121,7 @@ function computeFreqFromSpectrum(spectrum) {
 
 function drawReconstruction(frequency) {
     strokeWeight(3);
-    stroke(245, 158, 66);
+    stroke(reconstructionColor);
 
     let phase = Math.floor(freqSlider.value() / (0.5 * sampleRateSlider.value())) * Math.PI;
     if (frequency === freqSlider.value()) phase = 0; // need correction for when detected frequency is exactly 
@@ -115,8 +129,36 @@ function drawReconstruction(frequency) {
     drawSineWave(frequency, phase);
 }
 
+function drawInfo(detectedFrequency) {
+    strokeWeight(0);
+    fill(255);
+    textSize(15);
+    textFont('Helvetica');
+    textAlign(LEFT, CENTER);
+    text(`Samples per period: ${(sampleRateSlider.value() / freqSlider.value()).toFixed(2)}`, width * 0.1, height * 0.4)
+    text(`Detected frequency: ${detectedFrequency} Hz`, width * 0.1, height * 0.4 + 20);
+}
+
 function drawLegend() {
-
     stroke('white');
+    strokeWeight(sampleStrokeWeight);
+    point(width * 0.9 + 5, height * 0.4);
 
+    strokeWeight(0);
+    fill(255);
+    textAlign(LEFT, CENTER);
+    textSize(15);
+    textFont('Helvetica');
+
+    text('Sample', width * 0.9 + 20, height * 0.4);
+    text('Input Signal', width * 0.5 + 55, height * 0.4);
+    text('Reconstructed Signal', width * 0.7 + 25, height * 0.4);
+
+    textFont('Courier New');
+    textSize(80);
+    fill(inputColor);
+    text('~', width * 0.5, height * 0.4 + 2);
+
+    fill(reconstructionColor);
+    text('~', width * 0.7 - 30, height * 0.4 + 2);
 }
